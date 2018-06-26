cd72719ae4c7a7540a4c2673eca2625b89c20e68
Pamela Frieze <pfrieze@gmail.com>
2018-06-26 18:45:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ class App < Sinatra::Base
     "Goodbye, #{@persons_name}."
   end
 
-  get "/multiply/:num1,:num2" do
+  get "/multiply/:num1/:num2" do
     @multiplied = params[:num1 * :num2]
     @multiplied
   end
